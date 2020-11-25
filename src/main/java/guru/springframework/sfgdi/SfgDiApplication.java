4c7a5dd7c4bc759651fb0dbebcd8f4bdaa28d2e3
Amin <amin.rezazadeh93@gmail.com>
2020-11-25 22:28:42 +0330

    adding spring dependency injection

--- src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
+++ src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
@@ -1,6 +1,9 @@
 package guru.springframework.sfgdi;
 
+import guru.springframework.sfgdi.controllers.ConstructorInjectionController;
 import guru.springframework.sfgdi.controllers.MyController;
+import guru.springframework.sfgdi.controllers.PropertyInjectionController;
+import guru.springframework.sfgdi.controllers.SetterInjectionController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -16,6 +19,21 @@ public class SfgDiApplication {
 		String greeting = myController.sayHello();
 
 		System.out.println(greeting);
+
+		System.out.println("------------------- property");
+
+		PropertyInjectionController propertyInjectionController = (PropertyInjectionController) ctx.getBean("propertyInjectionController");
+		System.out.println(propertyInjectionController.getGreetingService());
+
+		System.out.println("--------------- Setter");
+
+		SetterInjectionController setterInjectionController = (SetterInjectionController) ctx.getBean("setterInjectionController");
+		System.out.println(setterInjectionController.getGreetingService());
+
+		System.out.println("------------ Constructor");
+		ConstructorInjectionController constructorInjectionController = (ConstructorInjectionController) ctx.getBean("constructorInjectionController");
+		System.out.println(constructorInjectionController.getGreetingService());
+
 	}
 
 }
